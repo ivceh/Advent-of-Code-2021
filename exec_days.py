c739435871eba4706f1c27812506f133c803f234
ivceh <ivan.ceh1234@gmail.com>
2021-11-30 17:50:14 +0100

    Clean main.py

---
+++ exec_days.py
@@ -0,0 +1,28 @@
+import time, AoC2021_01
+days = [AoC2021_01.Day01]
+
+
+def infile(i):
+    return "inputs/input" + f"{i:02d}" + ".txt"
+
+
+def exec_day(i):
+    start_time = time.time()
+    print("Day" + f"{i:02d}")
+    days[i - 1](infile(i))
+    end_time = time.time()
+    time_diff = (end_time - start_time)
+    print(f"{time_diff:.6f}", "s")
+
+
+def exec_all():
+    for i in range(len(days)):
+        exec_day(i + 1)
+        print()
+
+
+def exec(day = None):
+    if day is None:
+        exec_all()
+    else:
+        exec_day(day)
--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-import time, AoC2021_01
-days = [AoC2021_01.Day01]
+from exec_days import exec
 
-
-def infile(i):
-    return "inputs/input" + f"{i:02d}" + ".txt"
-
-
-def exec_day(i):
-    start_time = time.time()
-    print("Day" + f"{i:02d}")
-    days[i-1](infile(i))
-    end_time = time.time()
-    time_diff = (end_time - start_time)
-    print(f"{time_diff:.6f}", "s")
-
-
-def exec_all():
-    for i in range(len(days)):
-        exec_day(i + 1)
-        print()
-
-
-exec_day(1)
+exec()
