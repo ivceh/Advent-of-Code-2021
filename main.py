cfe4219d0fc97a4d3cdf499cb83244ddcbcfe645
ivceh <ivan.ceh1234@gmail.com>
2021-11-30 16:43:47 +0100

    Measure execution time in µs

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import AoC2021_01
-
+import time, AoC2021_01
 days = [AoC2021_01.Day01]
 
 
@@ -8,8 +7,12 @@ def infile(i):
 
 
 def exec_day(i):
+    start_time = time.time()
     print("Day" + f"{i:02d}")
     days[i-1](infile(i))
+    end_time = time.time()
+    time_diff = (end_time - start_time)
+    print(f"{time_diff:.6f}", "s")
 
 
 def exec_all():
