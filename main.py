705d97888aec79fff2ce2edf238c69b1d3d93781
ivceh <ivan.ceh1234@gmail.com>
2021-12-01 11:19:08 +0100

    exec_days() examples

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from exec_days import exec_days
 
 exec_days()
+# examples:
+#  exec_days()        # executes all days
+#  exec_days(1)       # executes day01("inputs/input01.txt")
+#  exec_days(2, "b")  # executes day02("inputs/input02_b.txt")
