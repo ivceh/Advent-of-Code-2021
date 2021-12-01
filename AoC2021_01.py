cb7dd9cbcfca409f1f8617a00c2732770e136ceb
ivceh <ivan.ceh1234@gmail.com>
2021-12-01 11:28:29 +0100

    Use islice to avoid creating new lists

--- AoC2021_01.py
+++ AoC2021_01.py
@@ -1,17 +1,21 @@
+from itertools import islice
+
 def day01(infile):
     with open(infile, "r") as file:
         A = [int(l) for l in file.read().splitlines()]
 
     # resolving Part One
     cnt1 = 0
-    for n, m in zip(A[:-1], A[1:]):
+    for n, m in zip(islice(A, 0, len(A) - 1),
+                    islice(A, 1, None)):
         if n < m:
             cnt1 += 1
     print("Part One: ", cnt1)
 
     # resolving Part Two
     cnt2 = 0
-    for n, m in zip(A[:-3], A[3:]):
+    for n, m in zip(islice(A, 0, len(A) - 3),
+                    islice(A, 3, None)):
         if n < m:
             cnt2 += 1
     print("Part Two: ", cnt2)
