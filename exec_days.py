267ec9bc4bc1e194fbf2f9d3d4e38528b6bd9aca
ivceh <ivan.ceh1234@gmail.com>
2021-12-01 06:40:59 +0100

    Enable additional test input files

--- exec_days.py
+++ exec_days.py
@@ -2,14 +2,16 @@ import time, AoC2021_01
 days = [AoC2021_01.day01]
 
 
-def infile(i):
-    return "inputs/input" + f"{i:02d}" + ".txt"
+def infile(i, input_add = None):
+    return "inputs/input" + f"{i:02d}" +\
+           ("" if input_add is None else "_" + input_add) +\
+           ".txt"
 
 
-def exec_day(i):
+def exec_day(i, input_add = None):
     start_time = time.time()
     print("Day" + f"{i:02d}")
-    days[i - 1](infile(i))
+    days[i - 1](infile(i, input_add))
     end_time = time.time()
     time_diff = (end_time - start_time)
     print(f"{time_diff:.6f}", "s")
@@ -21,8 +23,8 @@ def exec_all():
         print()
 
 
-def exec_days(day = None):
+def exec_days(day = None, input_add = None):
     if day is None:
         exec_all()
     else:
-        exec_day(day)
+        exec_day(day, input_add)
