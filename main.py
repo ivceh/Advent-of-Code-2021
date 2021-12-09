f63c75a952c33e72f68d20a3ae501be184df5916
ivceh <ivan.ceh1234@gmail.com>
2021-12-09 11:29:24 +0100

    Solve day04

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ exec_days()
 # examples:
 #  exec_days()        # executes all days
 #  exec_days(1)       # executes day01("inputs/input01.txt")
-#  exec_days(2, "b")  # executes day02("inputs/input02_b.txt")
+#  exec_days(2, "B")  # executes day02("inputs/input02_b.txt")
